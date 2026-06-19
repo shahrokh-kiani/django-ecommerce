dea7846a7bf9e5093ae074fc02b5800b42c0a052
Shahrokh Kiani <shahrokhkiani@proton.me>
2026-06-19 07:16:46 +0330

    fixed landing page showing no product

--- homeappliance/core/views.py
+++ homeappliance/core/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Avg
+from shop.models import Product
 
 
 def landing(request):
-    return render(request, "landing.html")
+    top_products = Product.objects.annotate(
+        avg_rating=Avg('ratings__score')
+    ).order_by('-id')[:8]
+    return render(request, "landing.html", {"top_products": top_products})
 
 
 def aboutus(request):
